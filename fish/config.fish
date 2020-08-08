72a6e9a24ea008f83654a205f778c59139a59209
Szymon Goldberg <szymigold@pop-os.localdomain>
2020-08-08 20:34:51 +0200

    Added fish.config file

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+setxkbmap -option caps:escape
+alias clangdante='clang -ggdb3 -std=c11 -x c -fdiagnostics-color -pedantic -Werror -Wall -Wextra -Wno-error=parentheses -Wno-parentheses -o'
+alias del='rm -rf'
+alias gst='git status'
+alias rename='mv'
+alias py3='python3'
