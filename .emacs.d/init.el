1b2c44b95b64830ff8686208e056e418f9c67a7b
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2025-06-19 23:46:45 +0200

    updated emacs init file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -29,8 +29,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("51fa6edfd6c8a4defc2681e4c438caf24908854c12ea12a1fbfd4d055a9647a3" default))
- '(package-selected-packages '(gruvbox-theme)))
+   '("51fa6edfd6c8a4defc2681e4c438caf24908854c12ea12a1fbfd4d055a9647a3"
+     default))
+ '(org-agenda-files
+   '("~/inne/orgfiles/todo.org"
+     "/Users/szymongorka/inne/orgfiles/til.org"))
+ '(package-selected-packages
+   '(counsel doom-modeline gruvbox-theme ivy ivy-rich swiper which-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -38,4 +43,49 @@
  ;; If there is more than one, they won't work right.
  )
 
-(load-theme 'gruvbox-dark-soft)
+ (load-theme 'gruvbox-dark-soft)
+
+(require 'org-tempo)
+
+
+(use-package swiper :ensure t)
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         :map ivy-switch-buffer-map
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(column-number-mode)
+(global-display-line-numbers-mode 1)
+
+(dolist (mode '(term-mode-hook
+                shell-mode-hook
+                treemacs-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(use-package which-key
+  :defer 0
+  :diminish which-key-mode
+  :config
+  (which-key-mode)
+  (setq which-key-idle-delay 0.5))
+
+(setq org-export-html-with-ids nil)
+
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
