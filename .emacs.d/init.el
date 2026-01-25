bb9e7b8d4d24be832d9334a316962e09dd19b581
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2026-01-26 00:38:29 +0100

    Updated emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,6 +8,8 @@
                          ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
+(set-face-attribute 'default nil :font "JetBrains Mono" :height 220)
+
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
@@ -35,7 +37,8 @@
    '("~/inne/orgfiles/todo.org"
      "/Users/szymongorka/inne/orgfiles/til.org"))
  '(package-selected-packages
-   '(counsel doom-modeline gruvbox-theme ivy ivy-rich swiper which-key)))
+   '(counsel doom-modeline gruvbox-theme ivy ivy-rich org-bullets
+	     org-roam org-roam-ui swiper visual-fill-column which-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -83,9 +86,46 @@
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
          ("C-x b" . counsel-ibuffer)
+	 ("C-M-j" . 'counsel-switch-buffer)
          ("C-x C-f" . counsel-find-file)))
 
 (use-package ivy-rich
   :init
   (ivy-rich-mode 1))
 
+
+(use-package org)
+
+(use-package org-bullets
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+
+
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "~/inne/roam"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  (org-roam-db-autosync-mode)
+  (setq org-roam-graph-link-hidden-types '("file" "http" "https")
+	org-roam-graph-extra-config '(("rankdir" . "LR"))))
+
+
+(use-package org-roam-ui
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
+
+
+(setq org-image-actual-width (list 600))
